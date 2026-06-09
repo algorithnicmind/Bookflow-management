c0482a4553635a5d02323e5bdb6041e901f79e14
algorithnicmind <ankitsahoo885@gmail.com>
2026-06-09 11:56:56 +0530

    updating the seed.py
    Co-Authored-By: Pragyan Paramita Moharana <moharanapragyanparamita@gmail.com>

--- server/db/seed.py
+++ server/db/seed.py
@@ -15,7 +15,7 @@ from app.core.config import settings
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 async def seed_data():
-    engine = create_async_engine(settings.DATABASE_URL, echo=True)
+    engine = create_async_engine(settings.async_database_url, echo=True)
     
     async_session = async_sessionmaker(engine, expire_on_commit=False)
     
